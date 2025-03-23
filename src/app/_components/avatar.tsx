f51cb1c338f41829920db880482c0e6c2e9a3067
MrLolok <lolok3221@gmail.com>
2025-03-23 21:56:21 +0100

    Sistemate le dimensioni dell'immagine della scimmia Modifica del padding verticale Aggiustata altezza calcolata sulla lunghezza dell'area visibile

--- src/app/_components/avatar.tsx
+++ src/app/_components/avatar.tsx
@@ -6,9 +6,9 @@ export function Avatar() {
             id="avatar"
             src="/monkey-head.webp"
             alt="Chef"
-            width={384}
+            width={300}
             height={0}
-            className="max-w-[192px] md:max-w-[256px]"
+            className="max-w-[200px] md:max-w-[250px] xl:max-w-[300px]"
             style={{ width: '100%', height: 'auto' }}
         />
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Chat } from "./_components/chat";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-full max-h-[100vh] overflow-hidden flex flex-col gap-8 justify-items-start items-center max-w-md p-8">
+    <div className="relative w-full h-full overflow-hidden flex flex-col gap-8 justify-items-start items-center max-w-md p-8 xl:py-12 2xl:py-16">
       <Chat/>
     </div>
   );
